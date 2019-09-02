98de7c1b624f57b8306c06ef91ad7cb3bb994278
Evan.G <seazeg@126.com>
2019-09-02 11:23:31 +0800

    Default Commit Message :art:

--- src/web/layout/components/Navbar.vue
+++ src/web/layout/components/Navbar.vue
@@ -36,14 +36,6 @@
               {{ $t('navbar.dashboard') }}
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/seazeg/vue-web-client-admin/">
-            <el-dropdown-item>
-              {{ $t('navbar.github') }}
-            </el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://seazeg.github.io/vue-web-client-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
           <el-dropdown-item divided>
             <span style="display:block;" @click="logout">{{ $t('navbar.logOut') }}</span>
           </el-dropdown-item>
